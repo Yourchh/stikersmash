5563108b8cf0e41b1577371505a773ab42d21238
Jorge Hernandez <hu220111169@lahuerta.tecmm.edu.mx>
2026-04-16 23:14:32 -0600

    icono implementado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,25 @@
 import { Stack } from "expo-router";
-
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+
+// 1. Evita que la pantalla de carga se oculte automáticamente al instante
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useEffect(() => {
+    // 2. Configura un temporizador para ocultar el splash screen después de 3 segundos (3000 ms)
+    const hideSplashScreen = async () => {
+      const timer = setTimeout(async () => {
+        await SplashScreen.hideAsync();
+      }, 3000);
+
+      return () => clearTimeout(timer); // Limpieza por seguridad
+    };
+
+    hideSplashScreen();
+  }, []);
+
   return (
     <>
       <Stack>
